173e8ab4575c3614b1c58cca97234f60157bd9c1
onofre <brunoonofre64@gmail.com>
2022-04-27 23:44:28 -0300

    desafio web service calculadora

--- exercicios-sb/src/main/java/br/com/cod3r/exerciciossb/controllers/ClienteController.java
+++ exercicios-sb/src/main/java/br/com/cod3r/exerciciossb/controllers/ClienteController.java
@@ -1,7 +1,9 @@
 package br.com.cod3r.exerciciossb.controllers;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import br.com.cod3r.exerciciossb.models.Cliente;
@@ -9,10 +11,20 @@ import br.com.cod3r.exerciciossb.models.Cliente;
 @RestController
 @RequestMapping(path = "/clientes")
 public class ClienteController {
-	
+
 	@GetMapping(path = "/qualquer")
 	public Cliente obterCliente() {
 		return new Cliente(23, "Bruno", "097.983.189-00");
+	}
+
+	@GetMapping("/{id}")
+	public Cliente obterClientePorId1(@PathVariable int id) {
+		return new Cliente(id, "Maria", "098.984.567-00");
+	}
+
+	@GetMapping
+	public Cliente obterClientePorId2(@RequestParam(name = "id", defaultValue = "1") int id) {
+		return new Cliente(id, "Joao", "068.964.667-60");
 	}
 
 }
